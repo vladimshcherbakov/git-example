ad8c49c9af2eab34df4dc3f641a146584358345a
Vladimir Shcherbakov <shcherbakov-01@mail.ru>
2022-10-13 15:34:32 +0700

    added an empty line

--- Src/Main.java
+++ Src/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
 
         SimpleDateFormat formatter = new SimpleDateFormat("yyyy/MMM/dd");
+        System.out.println();
         System.out.println(" Текущая дата: " + formatter.format(new Date()));
     }
 }
