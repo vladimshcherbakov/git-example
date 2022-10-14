8968ca4c47d0bfddcad3f91668586dc9cc50955e
Vladimir Shcherbakov <shcherbakov-01@mail.ru>
2022-10-14 11:43:17 +0700

    ?

--- Src/Main.java
+++ Src/Main.java
@@ -4,11 +4,7 @@ import java.util.Date;
 public class Main {
     public static void main(String[] args) {
 
-<<<<<<< Updated upstream
         SimpleDateFormat formatter = new SimpleDateFormat("yyyy/MMM/dd");
-=======
-        SimpleDateFormat formatter = new SimpleDateFormat("/MMM/dd");
->>>>>>> Stashed changes
         System.out.println();
         System.out.println(formatter.format(new Date()));
     }
