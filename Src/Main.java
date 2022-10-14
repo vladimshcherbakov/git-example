e7ba870ca5b0ee07c8a45023e86a7d2f20bfc2f5
Vladimir Shcherbakov <shcherbakov-01@mail.ru>
2022-10-14 11:14:53 +0700

    added an empty line

--- Src/Main.java
+++ Src/Main.java
@@ -4,7 +4,12 @@ import java.util.Date;
 public class Main {
     public static void main(String[] args) {
 
+<<<<<<< Updated upstream
         SimpleDateFormat formatter = new SimpleDateFormat("yyyy/MMM/dd");
+=======
+        SimpleDateFormat formatter = new SimpleDateFormat("/MMM/dd");
+        System.out.println();
+>>>>>>> Stashed changes
         System.out.println(formatter.format(new Date()));
     }
 }
