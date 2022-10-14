16f24c25f2b38f9c991bf39f1952ae154d280ced
Vladimir Shcherbakov <shcherbakov-01@mail.ru>
2022-10-14 11:30:07 +0700

    merge

--- Src/Main.java
+++ Src/Main.java
@@ -4,12 +4,9 @@ import java.util.Date;
 public class Main {
     public static void main(String[] args) {
 
-<<<<<<< Updated upstream
         SimpleDateFormat formatter = new SimpleDateFormat("yyyy/MMM/dd");
-=======
-        SimpleDateFormat formatter = new SimpleDateFormat("/MMM/dd");
         System.out.println();
->>>>>>> Stashed changes
+        System.out.println("Дата");
         System.out.println(formatter.format(new Date()));
     }
 }
