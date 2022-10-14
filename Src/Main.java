a6d66e773c731de2c3f4769843493c544cabc71a
Vladimir Shcherbakov <shcherbakov-01@mail.ru>
2022-10-14 11:00:12 +0700

    to discard changes in working directory

--- Src/Main.java
+++ Src/Main.java
@@ -5,7 +5,6 @@ public class Main {
     public static void main(String[] args) {
 
         SimpleDateFormat formatter = new SimpleDateFormat("yyyy/MMM/dd");
-        System.out.println();
         System.out.println(" Текущая дата: " + formatter.format(new Date()));
     }
 }
